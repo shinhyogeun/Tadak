8bdf28f1086ac8c8c02dfc151a3bec4c4ec6e731
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-01-19 01:53:32 +0900

    1/19 feat : 관리자-게임 타이틀 올리기 성공

--- Tadak/ViewController/ADMINViewController.swift
+++ Tadak/ViewController/ADMINViewController.swift
@@ -9,14 +9,41 @@ import UIKit
 import Firebase
 
 class ADMINViewController: UIViewController {
-
+    
+    let ref = Database.database().reference()
+    var category: String = ""
+    @IBOutlet weak var inputTitle: UITextField!
+    @IBOutlet weak var inputGameText: UITextField!
+    @IBOutlet weak var showGameText: UITextView!
+    @IBOutlet weak var showCategory: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         hideKeyboard()
-
     }
     
-
+    @IBAction func addLineButton(_ sender: RoundButton) {
+        
+    }
+    
+    @IBAction func korButton(_ sender: RoundButton) {
+        category = "한글대전"
+        showCategory.text = category
+    }
+    @IBAction func engButton(_ sender: RoundButton) {
+        category = "영어대전"
+        showCategory.text = category
+    }
+    @IBAction func extButton(_ sender: UIButton) {
+        category = "Extra"
+        showCategory.text = category
+    }
+    
+    @IBAction func uploadButton(_ sender: RoundButton) {
+        uploadTitle()
+        
+    }
+    
     @IBAction func backButton(_ sender: UIButton) {
         backtoMainView()
     }
@@ -30,5 +57,18 @@ extension ADMINViewController {
         vcName?.modalPresentationStyle = .fullScreen
         vcName?.modalTransitionStyle = .crossDissolve
         self.present(vcName!, animated: true, completion: nil)
+    }
+    
+    func uploadTitle() {
+        DispatchQueue.main.async {
+            self.ref.child("gametitle").child(self.category).observeSingleEvent(of: .value) { (snapshot) in
+                
+                let index = Int(snapshot.childrenCount)
+                let title: String = self.inputTitle.text!
+                let data = [String(index): title]
+                
+                self.ref.child("gametitle").child(self.category).updateChildValues(data)
+            }
+        }
     }
 }

302ab0e40d73a5ca347e84ba40ca0c3e9a29dce9
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-01-19 14:35:25 +0900

    1/19 feat : 설정창-각 버튼 경고창 기본틀 구현

--- Tadak/ViewController/SettingViewController.swift
+++ Tadak/ViewController/SettingViewController.swift
@@ -19,9 +19,15 @@ class SettingViewController: UIViewController {
         getNickName()
     }
 
+    @IBAction func changeNickName(_ sender: RoundButton) {
+        changeNickName()
+    }
     @IBAction func logout(_ sender: RoundButton) {
-        Logout.tryLogout()
-        goStartView()
+        logout()
+    }
+    
+    @IBAction func deleteUser(_ sender: RoundButton) {
+        deleteUser()
     }
     
     @IBAction func ADMIN(_ sender: UIButton) {
@@ -55,5 +61,52 @@ extension SettingViewController {
         vcName?.modalPresentationStyle = .fullScreen
         vcName?.modalTransitionStyle = .crossDissolve
         self.present(vcName!, animated: true, completion: nil)
+    }
+    
+    func logout() {
+        print("logout")
+        let alert = UIAlertController(title:"로그아웃 하시겠습니까?",message: "",preferredStyle: UIAlertController.Style.alert)
+        let cancle = UIAlertAction(title: "취소", style: .default, handler: nil)
+        let ok = UIAlertAction(title: "확인", style: .destructive, handler: {
+            action in
+            Logout.tryLogout()
+            self.goStartView()
+        })
+        alert.addAction(cancle)
+        alert.addAction(ok)
+        present(alert,animated: true,completion: nil)
+    }
+    
+    func deleteUser() {
+        print("deleteUser")
+        let alert = UIAlertController(title:"계정을 삭제하시겠습니까?",message: "",preferredStyle: UIAlertController.Style.alert)
+        let cancle = UIAlertAction(title: "취소", style: .default, handler: nil)
+        let ok = UIAlertAction(title: "확인", style: .destructive, handler: {
+            action in
+            //일단은 로그아웃 기능과 동일
+            Logout.tryLogout()
+            self.goStartView()
+        })
+        alert.addAction(cancle)
+        alert.addAction(ok)
+        present(alert,animated: true,completion: nil)
+    }
+    
+    func changeNickName() {
+        print("changeNickName")
+        let alert = UIAlertController(title: showName.text, message: "10자리 내의 새로운 닉네임을 입력해주세요", preferredStyle: .alert)
+        let cancle = UIAlertAction(title: "취소", style: .default, handler: nil)
+        let ok = UIAlertAction(title: "확인", style: .destructive, handler: {
+            action in
+            print(alert.textFields?[0].text ?? "")
+        })
+        alert.addTextField { (inputNewNickName) in
+            inputNewNickName.placeholder = "새로운 닉네임 입력"
+            inputNewNickName.textAlignment = .center
+            inputNewNickName.font = UIFont(name: "NanumSquareRoundR", size: 17)
+        }
+        alert.addAction(cancle)
+        alert.addAction(ok)
+        present(alert,animated: true,completion: nil)
     }
 }
